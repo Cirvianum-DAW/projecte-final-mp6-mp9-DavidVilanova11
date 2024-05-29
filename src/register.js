7a0fd1bf414b7c784c2a0264664de51c964268ae
daw25 <navjashan.singh@cirvianum.cat>
2024-05-29 17:28:27 +0200

    register correcte

---
+++ src/register.js
@@ -0,0 +1,78 @@
+async function register(email, contrasenya) {
+    const apiUrl = 'https://6644bc3cb8925626f88fb766.mockapi.io/api/vins/usuaris';
+  
+    try {
+      // Verificar si ya existe un usuario con el mismo email
+      const usuariosResponse = await fetch(apiUrl + `?search=${email}`);
+      if (!usuariosResponse.ok) {
+        if (usuariosResponse.status === 404) {
+          // Si no se encuentra ningún usuario, continuar con el registro
+          return null;
+        }
+        throw new Error('Error al buscar el usuario');
+      }
+  
+      const usuariosData = await usuariosResponse.json();
+  
+      if (usuariosData.length > 0) {
+        throw new Error('Ya existe un usuario con este email');
+      }
+  
+      // Si no hay usuarios con el mismo email, proceder con el registro
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          contrasenya: contrasenya,
+          vinsPreferits: []
+        })
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al registrar el usuario');
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Ha ocurrido un error al registrar el usuario:', error);
+      throw error;
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const registerForm = document.getElementById('registerForm');
+  
+    if (registerForm) {
+      registerForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+  
+        const email = document.getElementById('email').value;
+        const contrasenya = document.getElementById('password').value;
+  
+        try {
+          if (!email || !contrasenya) {
+            throw new Error('Por favor, rellena todos los campos');
+          }
+  
+          const registrado = await register(email, contrasenya);
+          
+          if (registrado !== null) {
+            console.log('Usuario registrado correctamente:', registrado);
+            alert('Usuario registrado correctamente');
+          } else {
+            console.log('No se encontró ningún usuario con este email, continuando con el registro...');
+            // Continuar con el proceso de registro
+            // No se muestra un mensaje de error ya que no se encontró ningún usuario durante la búsqueda
+          }
+        } catch (error) {
+          console.error('Ha ocurrido un error al registrar el usuario:', error);
+          alert(error.message);
+        }
+      });
+    }
+  });
+  
